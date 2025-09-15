90ae82fcba233de4d905b6b736f7e1f770751a0a
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-09-15 16:11:32 +0000

    color 1

--- done.js
+++ done.js
@@ -3,7 +3,10 @@
 /* COLOR KATAS */
 
 /* No Prompt */
-
+// make a string variable named color, set it equal to your favorite color.
+var color = "blue";
+// alert the message" "My favorite color is " plus the variable name, outside the quotes.
+alert("My favorite color is " + color);
 
 /* Prompt */
 
